160f95d794025031f1591f4caa7a6edb3661dfad
HermanDahlen <hman.dahlen@gmail.com>
2017-10-12 12:43:45 +0200

    added ui-router

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
 import angular from "angular";
+import uirouter from "angular-ui-router";
+import routes from "./routes";
 
-const DerpStone = angular.module("DerpStone", []);
+const DerpStone = angular.module("DerpStone", [uirouter]).config(routes);

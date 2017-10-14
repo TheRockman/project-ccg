6d42bbe87e54f3a679cb18deeea2907199450665
HermanDahlen <hman.dahlen@gmail.com>
2017-10-14 13:08:36 +0200

    fetch and create cards

---
+++ src/deck/deck.service.js
@@ -0,0 +1,144 @@
+export default function fetchCards() {
+  return [
+    {
+      available: "sick",
+      rules: ["Haste"],
+      name: "Bounty Hunter",
+      ap: 5,
+      hp: 2,
+      desc: "Attacks the enemy when hired",
+      cost: 9,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: ["Rage"],
+      name: "Town Drunk",
+      ap: 4,
+      hp: 2,
+      desc: "Attacks all minions when hired",
+      cost: 6,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: ["Undead"],
+      name: "Veteran",
+      ap: 2,
+      hp: 1,
+      desc: "Regain 1 hp when at 0 hp",
+      cost: 3,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: ["Battleborn"],
+      name: "Guttersnipe",
+      ap: 1,
+      hp: 5,
+      desc: "Can attack immediately",
+      cost: 2,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: ["Holy"],
+      name: "Cutthroat",
+      ap: 2,
+      hp: 4,
+      desc: "Immune to wounded attackers",
+      cost: 3,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: [],
+      name: "Oarssman",
+      ap: 5,
+      hp: 5,
+      desc: "",
+      cost: 4,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: ["Smuggler"],
+      name: "Angry Farmboy",
+      ap: 2,
+      hp: 1,
+      desc: "Draw a random card when hiered",
+      cost: 2,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: [],
+      name: "Noble Knave",
+      ap: 3,
+      hp: 2,
+      desc: "",
+      cost: 5,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: ["Martyr"],
+      name: "Mad Cultist",
+      ap: 0,
+      hp: 2,
+      desc: "Heals all allies and is wounded",
+      cost: 7,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: [],
+      name: "Meager Barkeep",
+      ap: 2,
+      hp: 5,
+      desc: "",
+      cost: 4,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: [],
+      name: "Restless Coachman",
+      ap: 5,
+      hp: 2,
+      desc: "",
+      cost: 6,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: ["Holy"],
+      name: "Weary Monk",
+      ap: 1,
+      hp: 6,
+      desc: "Immune to wounded attackers",
+      cost: 4,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: ["Rage"],
+      name: "Shady Sellsword",
+      ap: 2,
+      hp: 6,
+      desc: "Attacks all minions when hired",
+      cost: 8,
+      img: "../img/card.png"
+    },
+    {
+      available: "sick",
+      rules: [],
+      name: "Salty Captain",
+      ap: 1,
+      hp: 2,
+      desc: "",
+      cost: 1,
+      img: "../img/card.png"
+    }
+  ];
+}
